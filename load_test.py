dc7eb496252896a9e7b124b20bf6022512ef693a
Prasanth495 <75207999+Prasanth495@users.noreply.github.com>
2021-12-08 10:47:20 +0530

    Add files via upload

---
+++ load_test.py
@@ -0,0 +1,21 @@
+from locust import HttpLocust, TaskSet, task
+
+class User(TaskSet):
+   def on_start(self):
+       self.client.post("/login", {"username":"admin", "password":"password"})
+ 
+   def on_stop(self):
+       self.client.post("/logout", {"username":"admin", "password":"password"})
+ 
+   @task(2)
+   def index(self):
+       self.client.get("/")
+ 
+   @task(1)
+   def test(self):
+       self.client.get("/test")
+ 
+class WebsiteUser(HttpLocust):
+   task_set = User
+   min_wait = 5000
+   max_wait = 9000
